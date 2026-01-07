829bed8475420c78713dbb9f8e177b3ab06e1dc8
Marcin Przybysz <marprzybysz888@gmail.com>
2026-01-07 17:47:52 +0000

    Fixed error with wrong counting on variable "user_trails_conter" and renamed to correct "user_trails_counter"

--- Gierka Liczbowa/main.py
+++ Gierka Liczbowa/main.py
@@ -2,24 +2,24 @@ import random
 
 # Zmienne podstawowe 
 random_number = random.randint(1, 10)
-user_trails = 5
-user_trails_conter = 0 
+user_trails = 7
+user_trails_counter = 1 
 
 # Input do gry
-user_guess = int(input(f"Zgadnij liczbę od 1 do 10: \n(Masz {user_trails} prób!)\n"))
+user_guess = int(input(f"Zgadnij liczbę od 1 do 10: \n(Masz {user_trails + 1} prób!)\nWpisz ponownie liczbę: "))
 
 # Pętla gry
-while user_trails > 0 and user_guess != random_number:
+while user_trails >= 1 and user_guess != random_number:
     user_trails -= 1
-    user_trails_conter += 1
+    user_trails_counter += 1 
     if user_guess < random_number:
         print("Za mała liczba! Spróbuj ponownie!")
-    elif user_guess > random_number:
+    else:
         print ("Za duża liczba Spróbuj ponownie!")
-    user_guess = int(input(f"Wpisz ponownie liczbę: \nPozostało: {user_trails}\n"))
+    user_guess = int(input(f"Pozostało: {user_trails + 1}\nWpisz ponownie liczbę: "))
 
 # Rozstrzygnięcie gry
 if user_trails >= 1:
-    print(f"Udało ci się zgadnąć liczbę w {user_trails_conter} próbach! Gratulacje :O")
+    print(f"Udało ci się zgadnąć liczbę w {user_trails_counter} próbach! Gratulacje :O")
 else:
     print(f"Niestety nie udało ci odgadnąć liczby. Prawidłowa liczba to {random_number}")
