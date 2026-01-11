99ff789395d55f5056be294ba79fb6aa235f63ac
Marcin Przybysz <marprzybysz888@gmail.com>
2026-01-11 12:25:00 +0000

    Prototype "input' line 11 main.py library.addbook()

--- Bilbioteka/main.py
+++ Bilbioteka/main.py
@@ -3,9 +3,9 @@ import classes/Book
 
 # Prosta baza danych na podstawie wywołania klas Library/Book
 
- 
 library = Library()
 library.add_book(Book("Pan Tadeusz", "Adam Mickiewicz"))
 library.add_book(Book("Quo vadis", "Henryk Siekiewicz"))
 library.display_books()
 
+library.add_book(Book(input("Wpisz tytuł ksiązki: "), "Adam Mickiewicz"))
