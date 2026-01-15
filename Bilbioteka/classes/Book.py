2d4fb9ac49e54133216a5394e4cbc69f1a90a241
Marcin Przybysz <marprzybysz888@gmail.com>
2026-01-15 19:54:20 +0000

    Added line self.id to identify current book

--- Bilbioteka/classes/Book.py
+++ Bilbioteka/classes/Book.py
@@ -3,9 +3,12 @@ Podstawowa klasa książki
 
 Założenia:
 Szablon który zawiera informacje ksiązki (autor, tytuł ksiażki)
+
+Dodanie id książki do indentyfikacji konkretnej ksiązki 
 ```
 
 class Book:
-    def __init__(self, title, author):
+    def __init__(self, title, author, book_id=None):
+        self.id = book_id
         self.title = title
         self.author = author
